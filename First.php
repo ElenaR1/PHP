dd13a96d6c78177fe22fd3e22cfb036d7f9b2f27
ElenaR1 <elena.radoycheva@abv.bg>
2018-10-14 13:58:09 +0300

    Create First.php

---
+++ First.php
@@ -0,0 +1,106 @@
+	
+  PARAHRAPHS AND HEADINGS
+  
+  <!doctype html
+
+<html>
+
+<head>
+
+<title> My Web Page </title>
+
+</head>
+
+<body>
+		<h1> The fish website </h1>
+		
+		<p> Welcome </p>
+		
+		<h2> Bass Fish </h2>
+		
+		<p> Bss fish live in ... </p>
+		
+</body>
+
+
+</html>
+
+  
+  
+  LISTS
+  <!--<ul>
+		
+		<li> CD </li>
+		<li> DVD </li>
+		<li> Blu Ray </li>
+		
+		</ul>
+
+		<ol>
+		
+		<li> CD </li>
+		<li> DVD </li>
+		<li> Blu Ray </li>
+		
+		</ol>
+		-->
+		
+		<!-- TABLE --> 
+		<table>
+		
+		<tr>
+			<th> <!--for columns-->
+			The Planets<!--col A-->
+			</th>
+			<th>
+			The Oceans
+			</th>
+			<th>
+			The Deserts
+			</th>
+		</tr>	
+		<tr>
+			<td> Earth </td> <!--it corespons with col A -->
+			<td> Pacific </td>
+			<td> Sahara </td>
+		</tr>
+		<tr>
+			<td> Pluto </td> <!--it corespons with col A -->
+			<td> Atlantic </td>
+			<td> Gobi </td>
+		</tr>
+		</table>
+    
+    
+ LINK to an outside page 
+    
+<body>
+<p> Please click on this link 
+<a href="https://www.youtube.com/watch?v=U4UHoiK6Oo4&index=6&list=PLr6-GrHUlVf_ZNmuQSXdS197Oyr1L9sPB" >
+Tutorial </a>
+</p>	
+</body>
+
+
+LINK WITHIN THE SAME WEB PAGE 
+<ul>
+
+	<li> <a href="#Birds"> Birds Section </a> </li>
+	<li> <a href="#Ducks"> Ducks Section </a> </li>
+	
+</ul>
+
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+
+<p> <a name="Birds"> This is the article about birds. </a> </p>
+<br>
+<br>
+<br>
+<br>
+
+<p> <a name="Ducks"> This is the article about ducks. </a> </p>
