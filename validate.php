6365f9865540d50dc3575773d99a68d4ae86b373
ElenaR1 <elena.radoycheva@abv.bg>
2018-11-09 14:59:39 +0200

    Add files via upload

---
+++ validate.php
@@ -0,0 +1,46 @@
+﻿<?php
+    require_once('validator.php');
+
+    $requirements = [
+        "courseID"      =>  [ "max" => 150 ],
+        "lecturer"      =>  [ "max" => 200 ],
+        "description"   =>  [ "min" => 10 ]
+    ];
+    $validate = new ValidateCourse($_POST, $requirements);
+    if(!$validate->isValid())
+    {
+        $validate->printError();
+    }
+    else
+    { 
+        $host   = "127.0.0.1";
+        $db     = "webhw";
+        $user   = "root";
+        $pass   = "";
+        $conn   = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+        
+        $conn->beginTransaction();
+        $checkCol = "SHOW COLUMNS FROM electives WHERE Field = 'created_at'";
+        $query = $conn->query($checkCol) or die("failed!");
+        if ($row = $query->fetch(PDO::FETCH_ASSOC))
+        {
+            // Column exists -> do nothing
+        }
+        else
+        {
+            // Column does not exists -> add it
+            $st = $conn->prepare("ALTER TABLE electives ADD created_at".
+                                        " TIMESTAMP DEFAULT CURRENT_TIMESTAMP");
+            $st->execute(); 
+        }
+        
+        $st = $conn->prepare("INSERT INTO electives (title, description, lecturer)" .
+                                " VALUES(?, ?, ?)");
+        $st->execute(array( $_POST["courseID"],
+                            $_POST["description"],
+                            $_POST["lecturer"],));
+        
+        $conn->commit();
+        echo "end";
+    }
+?>
---
+++ validator.php
@@ -0,0 +1,67 @@
+﻿<?php
+    abstract class Validate {
+        protected $invalid_field;
+        protected $isValid;
+        protected $fields;
+        protected $errors;
+        protected $requirements;
+        protected $data; 
+        public function __construct(array $data, array $requirements){
+            $this->data = $data;
+            $this->requirements = $requirements;
+            $this->fields = array_keys($data);
+            $this->errors = array();
+            $this->invalid_field = null;
+            $this->isValid = false;
+        }
+        
+        abstract public function isValid();
+        abstract public function getInvalidField();
+        abstract public function printError();	
+    };
+    
+    class ValidateCourse extends Validate {
+	
+        public function __construct(array $data, array $requirements){
+            parent::__construct($data, $requirements);
+        }
+        
+        public function isValid() {
+            $field = $this->fields[0];
+            if(strlen($this->data[$field]) > $this->requirements[$field]['max']) {
+                $this->errors[$field] =
+                                "$field има проблем. Дължината трябва да е по-малка от "
+                                . $this->requirements[$field]['max'];
+                $this->invalid_field = $field;
+            }
+            
+            $field = $this->fields[1];
+            if(strlen($this->data[$field]) > $this->requirements[$field]['max']) {
+                $this->errors[$field] =
+                                "$field има проблем. Дължината трябва да е по-малка от "
+                                . $this->requirements[$field]['max'];
+                $this->invalid_field = $field;
+            }
+            
+            $field = $this->fields[2];
+            if(strlen($this->data[$field]) < $this->requirements[$field]['min']) {
+                $this->errors[$field] =
+                                "$field има проблем. Дължината трябва да е по-голяма от "
+                                . $this->requirements[$field]['min'];
+                $this->invalid_field = $field;
+            }
+            
+            return empty($this->errors);
+        }
+        
+        public function getInvalidField() {
+            return (string)$this->invalid_field;
+        }
+        
+        public function printError() {
+            foreach($this->errors as $err){
+                echo "<p style=\"color:red\"> $err </p>"; 
+            }
+        }
+    };
+?>
